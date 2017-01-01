d89c7435dac0061d699d225aede5d78007fdd6b9
Manuel Venegas <mengasis@outlook.com>
2017-01-01 20:55:53 -0300

    Generada la funcion para realizar el juego

--- app/jobs/play_roulette_job.rb
+++ app/jobs/play_roulette_job.rb
@@ -1,7 +1,36 @@
 class PlayRouletteJob < ApplicationJob
-  queue_as :default
+    queue_as :default
 
-  def perform(*args)
-    # Do something later
-  end
+    def perform(*_args)
+
+    end
+
+    def play(dinero)
+        probSeleccion = rand(100)
+        probResultado = rand(100)
+
+        # Probabilidad de que seleccione un color
+        color = case probSeleccion
+                when 0...49 then 'Rojo'
+                when 49...98 then 'Negro'
+                when 98...100 then 'Verde'
+        end
+
+        # Probabilidad de que salga ese color
+        resultado = case probResultado
+                    when 0...49 then 'Rojo'
+                    when 49...98 then 'Negro'
+                    when 98...100 then 'Verde'
+        end
+
+        # Si el color seleccionado con el resultado son iguales, gana
+        if color === resultado
+            return dinero * 2 if resultado === 'Negro' || resultado === 'Rojo'
+
+            return dinero * 15 if resultado == 'Verde'
+
+        else
+            return dinero
+        end
+    end
 end
